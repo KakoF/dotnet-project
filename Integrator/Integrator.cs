09c8216574dccdc5589224f66da3cf6b74090f15
Kako Ferrare <kakoferrare87@gmail.com>
2021-12-08 16:06:40 -0300

    feat: implements integrator project

---
+++ Integrator/Integrator.cs
@@ -0,0 +1,76 @@
+﻿using Domain.Models;
+using Integrator.Interface;
+using Microsoft.Extensions.Configuration;
+using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator
+{
+    public class Integrator : IIntegrator
+    {
+        private readonly string _url;
+        public Integrator(IConfiguration configuration)
+        {
+            _url = configuration["API_WEBMOTORS"];
+        }
+        public async Task<IEnumerable<Brand>> GetBrand()
+        {
+            var client = new RestClient(_url);
+            var request = new RestRequest("/OnlineChallenge/Make", Method.GET);
+            var response = await client.ExecuteAsync<List<Brand>>(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(
+                    string.Format(
+                        "Error: {0} | {1} | {2}",
+                        response.StatusCode,
+                        response.ErrorMessage,
+                        response.Content
+                    ),
+                    response.ErrorException
+                );
+
+            return response.Data;
+        }
+
+        public async Task<IEnumerable<Model>> GetModel(int brandId)
+        {
+            var client = new RestClient(_url);
+            var request = new RestRequest("/OnlineChallenge/Model", Method.GET).AddQueryParameter("MakeID", brandId.ToString());
+            var response = await client.ExecuteAsync<List<Model>>(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(
+                    string.Format(
+                        "Error: {0} | {1} | {2}",
+                        response.StatusCode,
+                        response.ErrorMessage,
+                        response.Content
+                    ),
+                    response.ErrorException
+                );
+
+            return response.Data;
+        }
+
+        public async Task<IEnumerable<Domain.Models.Version>> GetVersion(int modelId)
+        {
+            var client = new RestClient(_url);
+            var request = new RestRequest("/OnlineChallenge/Version", Method.GET).AddQueryParameter("ModelID", modelId.ToString());
+            var response = await client.ExecuteAsync<List<Domain.Models.Version>>(request);
+            if (response.StatusCode != System.Net.HttpStatusCode.OK)
+                throw new Exception(
+                    string.Format(
+                        "Error: {0} | {1} | {2}",
+                        response.StatusCode,
+                        response.ErrorMessage,
+                        response.Content
+                    ),
+                    response.ErrorException
+                );
+
+            return response.Data;
+        }
+    }
+}
---
+++ Integrator/Interface/IIntegrator.cs
@@ -0,0 +1,15 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Integrator.Interface
+{
+    public interface IIntegrator
+    {
+        Task<IEnumerable<Brand>> GetBrand();
+        Task<IEnumerable<Model>> GetModel(int brandId);
+        Task<IEnumerable<Domain.Models.Version>> GetVersion(int modelId);
+    }
+}
